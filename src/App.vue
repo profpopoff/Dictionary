ae1c0d416fdcdb82e2accbda529f9ef2f4b334dd
Aleksei Ivanov <profpopoff@gmail.com>
2023-01-29 16:43:10 +0300

    Add dark theme

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-
+import DarkModeToggle from './components/DarkModeToggle.vue'
 </script>
 
 <template>
@@ -9,9 +9,17 @@
 
   <main>
     <h1>main</h1>
+    <DarkModeToggle />
   </main>
 </template>
 
-<style scoped>
+<style>
+@import url('./styles/reset.css');
+@import url('./styles/variables.css');
 
+body {
+  background-color: hsl(var(--background-color));
+  color: hsl(var(--text-color));
+  transition: color .3s ease-in-out, background-color .3s ease-in-out;
+}
 </style>
---
+++ src/components/DarkModeToggle.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { useDark, useToggle } from '@vueuse/core'
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
+</script>
+
+<template>
+   <button class="button" @click="toggleDark()">
+      Dark mode: {{ isDark }}
+   </button>
+</template>
+
+<style scoped>
+.button {
+   color: hsl(var(--accent-color));
+   background-color: hsl(var(--text-color));
+}
+</style>
