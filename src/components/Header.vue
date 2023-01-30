91710c26940f8e4ad04b698e353a941a896210fb
Aleksei Ivanov <profpopoff@gmail.com>
2023-01-30 12:43:58 +0300

    Create header component and add logo

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Logo from './icons/Logo.vue'
+</script>
+
+<template>
+   <header>
+      <Logo />
+   </header>
+</template>
+
+<style scoped>
+header {
+   padding-block: 2.5em;
+   display: flex;
+   justify-content: space-between;
+}
+</style>
