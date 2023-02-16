d4246efdd6304af6c0aa51b0a1bcef2b5918a231
Aleksei Ivanov <profpopoff@gmail.com>
2023-02-16 11:33:23 +0300

    Add loading indicator delay and fix error message Previously the loading indicator would appear whenever getWordInfo function was called, and it was in effect only for the time fetch was running. Now we check that the fetch takes more than 250 milliseconds before running the loading indicator, which now always lasts 750 milliseconds after starting.

--- src/App.vue
+++ src/App.vue
@@ -6,19 +6,38 @@ import AppWord from '@/components/word/AppWord.vue'
 import LoadingIndicator from './components/LoadingIndicator.vue'
 
 const wordInfo = ref('')
-const isLoading = ref(false)
+const isLoading = ref(true)
+const fetchError = ref({ status: false, word: '' })
 
-onMounted(() => {
+onMounted(async () => {
+  await delay(500)
   getWordInfo('dictionary')
 })
 
 const getWordInfo = async (word) => {
-  isLoading.value = true
-  wordInfo.value = ''
+  const fetchStart = Date.now()
   await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
     .then(response => response.json())
-    .then(data => wordInfo.value = data)
-  isLoading.value = false
+    .then(data => {
+      if (Array.isArray(data)) {
+        fetchError.value = { status: false, word: '' }
+        wordInfo.value = data
+      } else {
+        throw new Error
+      }
+    })
+    .catch(() => fetchError.value = { status: true, word })
+    .finally(() => isLoading.value = false)
+  const fetchTime = Date.now() - fetchStart
+  if (fetchTime > 250) {
+    isLoading.value = true
+    await delay(750)
+    isLoading.value = false
+  }
+}
+
+const delay = ms => {
+  return new Promise(resolve => setTimeout(resolve, ms))
 }
 </script>
 
@@ -26,9 +45,11 @@ const getWordInfo = async (word) => {
   <AppHeader class="container" />
   <main class="container">
     <SearchBar @get-word="getWordInfo" />
-    <h1 v-if="wordInfo.message">{{ wordInfo.message }}</h1>
-    <AppWord v-else-if="wordInfo[0]" :wordInfo="wordInfo[0]" />
+    <h2 class="error" v-if="fetchError.status">Sorry, but&nbsp;we couldn&rsquo;t find definitions for the word &laquo;<span>{{
+      fetchError.word
+    }}</span>&raquo;.</h2>
     <LoadingIndicator v-else-if="isLoading" />
+    <AppWord v-else-if="wordInfo[0]" :wordInfo="wordInfo[0]" />
   </main>
 </template>
 
@@ -62,5 +83,14 @@ main {
   display: flex;
   flex-direction: column;
   gap: 2em;
+}
+
+.error {
+  font-size: 1.7em;
+  text-align: center;
+}
+
+.error span {
+  color: hsl(var(--accent-color));
 }
 </style>
